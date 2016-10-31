f470a57f157c743ee0bbd5841500a76d362a7801
zalax303 <chenhao_0313@hotmail.com>
2016-10-31 21:25:23 +0800

    init

---
+++ src/week3/Book.scala
@@ -0,0 +1,45 @@
+package week3
+
+import java.lang.String
+
+/**
+  * Created by dsfs on 2016/10/31.
+  */
+class Book(t: String, a: List[String]) {
+  val title = t
+  val authors = a
+
+  def ==(book: Book) = {
+    title == book.title && authors == book.authors
+  }
+}
+
+object Book extends App {
+    def apply(t: String, a: List[String]) = {
+      new Book(t, a)
+    }
+
+    def find(books: List[Book]) = {
+      for {
+        b1 <- books
+        b2 <- books
+        if b1.title != b2.title
+        a1 <- b1.authors
+        a2 <- b2.authors
+        if a1 == a2
+      } yield a1
+    }
+
+    val books = List[Book](
+    Book("Structure and Interpretation of Computer Programs", List("Abelson, Harald", "Sussman, Gerald J.")),
+    Book("Introduction to Functional Programming", List("Bird, Richard", "Wadler, Phil")),
+    Book("Effective Java", List("Bloch, Joshua")),
+    Book("Java Puzzlers", List("Bloch, Joshua", "Gafter, Neal")),
+    Book("Programming in Scala", List("Odersky, Martin", "Spoon, Lex", "Venners, Bill"))
+    )
+
+    println(find(books))
+
+  }
+
+
