a44513cb0faba37c5aa01f4ebe54af00a112d951
zalax303 <chenhao_0313@hotmail.com>
2016-11-07 18:11:05 +0800

    init

---
+++ src/week4/Arugments.scala
@@ -0,0 +1,21 @@
+package week4
+
+/**
+  * Created by dsfs on 2016/11/7.
+  */
+object Arugments extends App {
+  def variableArg(g: String*) = {
+    g.foreach(x => println(x))
+  }
+
+  def printPerson(name: String, age: Int = 10) = {
+    println("name is %s , age is %d", name, age)
+    println(s"name is $name, age is $age")
+  }
+
+  variableArg("1", "2", "3", "4")
+  val k = Array("10", "11", "12", "13")
+  variableArg(k: _*) //把array中每个元素拆散,逐个传到String*中
+
+  printPerson(name = "john")
+}
